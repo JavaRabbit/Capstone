e0a0d66161020cf8a8c56a7a45a9c8dcb4563d08
Bonnie <kbonnie@gmail.com>
2015-01-28 12:18:25 -0800

    working, new user login page created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(subject_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: "Thank you for signing up!"
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def subject_params
+    params.require(:user).permit(:email, :password, :password_digest)
+  end
+end
