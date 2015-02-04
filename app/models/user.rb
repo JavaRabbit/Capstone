5ad4dc1703b072a51446ff28fcfe12eba6391e99
Bonnie <kbonnie@gmail.com>
2015-02-03 16:09:04 -0800

    working, many to many user_feeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :accounts
-  
+  has_many :user_feeds
+  has_many :feeds, through: :user_feeds
+
   validates :email, presence: true, uniqueness: true
   validates_uniqueness_of :email, format: {with: /@/}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203222045) do
+ActiveRecord::Schema.define(version: 20150203234910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,11 +35,20 @@ ActiveRecord::Schema.define(version: 20150203222045) do
     t.datetime "updated_at"
   end
 
+  create_table "user_feeds", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "feed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+
 
 end
