63aa735f39c13535757d45d8026593567b8d0354
Bonnie <kbonnie@gmail.com>
2015-02-10 16:51:51 -0800

    working on news entry model

--- app/models/news_entry.rb
+++ app/models/news_entry.rb
@@ -13,9 +13,30 @@ class NewsEntry < ActiveRecord::Base
       aa.published_at = entry.published
       aa.guid = entry.id
       aa.save
-
     end
   end
 
-  
+  def update_from_feed_continuously(feed_url, delay_interval = 15.second)
+    feed = Feedjira::Feed.fetch_and_parse("http://feeds.finance.yahoo.com/rss/2.0/headline?s=yhoo&region=US&lang=en-US")
+    add_entries(feed.entries)
+    loop do
+      sleep delay_interval
+      feed = Feedjira::Feed.update(feed)
+      add_entries(feed.new_entries)
+  end
+
+  private
+
+  def add_entries(entries)
+    entries.each do |entry|
+      aa = NewsEntry.new
+      aa.name = entry.title
+      aa.summary = entry.summary
+      aa.url = entry.url
+      aa.published_at = entry.published
+      aa.guid = entry.id
+      aa.save
+    end
+  end
+end
 end
